89057268abcb16066ba7ec27b9b62059efeb8b60
guillaume <guillaumeval.techer@gmail.com>
2018-12-25 15:35:44 +0100

    adds possibility to add elements to already existing list

--- src/App.js
+++ src/App.js
@@ -6,8 +6,10 @@ import "./App.css";
 import "./spacing.css";
 
 class App extends Component {
+
   constructor(props) {
     super(props);
+    this.addsElement = this.addsElement.bind(this);
     this.state = {
       contents: [
         { title: "Test1", elements: ["bla", "blo"] },
@@ -16,10 +18,17 @@ class App extends Component {
       ]
     };
   }
+
+  addsElement(i, text){
+    const contents = this.state.contents.slice();
+    contents[i].elements = contents[i].elements.concat(text);
+    this.setState({contents:contents});
+  }
+
   render() {
     return (
       <div className="App">
-        <div class="columns is-vcentered mhx">
+        <div className="columns is-vcentered mhx">
           <div className="column">
             <p className="title is-3 has-text-left mtm ">
               {" "}
@@ -30,19 +39,20 @@ class App extends Component {
             <AddsList />
           </div>
         </div>
-        <body>
+        
           <div className="columns is-multiline mhx">
-            {this.state.contents.map(element => (
-              <List title={element.title} elements={element.elements} />
+            {this.state.contents.map((element,index) => (
+              <List key={index} index={index} title={element.title} elements={element.elements} addsElement={this.addsElement} />
             ))}
           </div>
-        </body>
+        
       </div>
     );
   }
 }
 
 class List extends Component {
+
   render() {
     return (
       <div className="column is-4">
@@ -50,10 +60,10 @@ class List extends Component {
           <p className="pvs has-text-left title is-size-5">
             {this.props.title}
           </p>
-          {this.props.elements.map(element => (
-            <Element name={element} />
+          {this.props.elements.map((element,index) => (
+            <Element key={index} name={element} />
           ))}
-          <AddsElement />
+          <AddsElement parentIndex={this.props.index} addsElement={this.props.addsElement} />
         </div>
       </div>
     );
@@ -63,7 +73,7 @@ class List extends Component {
 class Element extends Component {
   render() {
     return (
-      <div className="card mvs  is-shadowless has-background-light">
+      <div className="card mvs is-shadowless has-background-light">
         <p className="has-text-left paxs ">{this.props.name} </p>
       </div>
     );
@@ -73,8 +83,8 @@ class Element extends Component {
 class AddsList extends Component {
   render() {
     return (
-      <form>
-      <div className="  field has-addons">
+      <form onSubmit={this.props.addsList}>
+      <div className="field has-addons">
         <div className="control is-expanded is-medium">
           <input className="input is-medium" type="text" placeholder="Add a list" />
         </div>
@@ -88,15 +98,32 @@ class AddsList extends Component {
 }
 
 class AddsElement extends Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.state = {text:""}
+  }
+
+  handleChange(e){
+    this.setState({text:e.target.value});
+  }
+
+  handleSubmit(e){
+    e.preventDefault();
+    this.props.addsElement(this.props.parentIndex, this.state.text);
+    this.setState({text:""})
+  }
+
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
       <div className="field has-addons">
         <div className="control is-expanded">
-          <input className="input " type="text" placeholder="Add an element" />
+          <input className="input" onChange={this.handleChange} type="text" placeholder="Add an element" />
         </div>
         <div className="control ">
-          <button className="button  is-info">+</button>
+          <button  type="Submit" className="button is-info">+</button>
         </div>
       </div>
       </form>
