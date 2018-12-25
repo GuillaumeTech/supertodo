986236f3c47de985bda0dface37ae5744242192d
guillaume <guillaumeval.techer@gmail.com>
2018-12-25 15:38:29 +0100

    can not add empty elements to list anymore

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import "./App.css";
 import "./spacing.css";
 
 class App extends Component {
-
   constructor(props) {
     super(props);
     this.addsElement = this.addsElement.bind(this);
@@ -19,10 +18,10 @@ class App extends Component {
     };
   }
 
-  addsElement(i, text){
+  addsElement(i, text) {
     const contents = this.state.contents.slice();
     contents[i].elements = contents[i].elements.concat(text);
-    this.setState({contents:contents});
+    this.setState({ contents: contents });
   }
 
   render() {
@@ -39,20 +38,24 @@ class App extends Component {
             <AddsList />
           </div>
         </div>
-        
-          <div className="columns is-multiline mhx">
-            {this.state.contents.map((element,index) => (
-              <List key={index} index={index} title={element.title} elements={element.elements} addsElement={this.addsElement} />
-            ))}
-          </div>
-        
+
+        <div className="columns is-multiline mhx">
+          {this.state.contents.map((element, index) => (
+            <List
+              key={index}
+              index={index}
+              title={element.title}
+              elements={element.elements}
+              addsElement={this.addsElement}
+            />
+          ))}
+        </div>
       </div>
     );
   }
 }
 
 class List extends Component {
-
   render() {
     return (
       <div className="column is-4">
@@ -60,10 +63,13 @@ class List extends Component {
           <p className="pvs has-text-left title is-size-5">
             {this.props.title}
           </p>
-          {this.props.elements.map((element,index) => (
+          {this.props.elements.map((element, index) => (
             <Element key={index} name={element} />
           ))}
-          <AddsElement parentIndex={this.props.index} addsElement={this.props.addsElement} />
+          <AddsElement
+            parentIndex={this.props.index}
+            addsElement={this.props.addsElement}
+          />
         </div>
       </div>
     );
@@ -84,14 +90,18 @@ class AddsList extends Component {
   render() {
     return (
       <form onSubmit={this.props.addsList}>
-      <div className="field has-addons">
-        <div className="control is-expanded is-medium">
-          <input className="input is-medium" type="text" placeholder="Add a list" />
+        <div className="field has-addons">
+          <div className="control is-expanded is-medium">
+            <input
+              className="input is-medium"
+              type="text"
+              placeholder="Add a list"
+            />
+          </div>
+          <div className="control">
+            <button className="button is-medium is-primary">+</button>
+          </div>
         </div>
-        <div className="control">
-          <button className="button is-medium is-primary">+</button>
-        </div>
-      </div>
       </form>
     );
   }
@@ -102,30 +112,39 @@ class AddsElement extends Component {
     super(props);
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-    this.state = {text:""}
+    this.state = { text: "" };
   }
 
-  handleChange(e){
-    this.setState({text:e.target.value});
+  handleChange(e) {
+    this.setState({ text: e.target.value });
   }
 
-  handleSubmit(e){
+  handleSubmit(e) {
     e.preventDefault();
-    this.props.addsElement(this.props.parentIndex, this.state.text);
-    this.setState({text:""})
+    if (this.state.text != "") {
+      this.props.addsElement(this.props.parentIndex, this.state.text);
+      this.setState({ text: "" });
+    }
   }
 
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
-      <div className="field has-addons">
-        <div className="control is-expanded">
-          <input className="input" onChange={this.handleChange} type="text" placeholder="Add an element" />
+        <div className="field has-addons">
+          <div className="control is-expanded">
+            <input
+              className="input"
+              onChange={this.handleChange}
+              type="text"
+              placeholder="Add an element"
+            />
+          </div>
+          <div className="control ">
+            <button type="Submit" className="button is-info">
+              +
+            </button>
+          </div>
         </div>
-        <div className="control ">
-          <button  type="Submit" className="button is-info">+</button>
-        </div>
-      </div>
       </form>
     );
   }
